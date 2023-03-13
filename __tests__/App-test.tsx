9ddbdfacef391a2c64dcd2ecce3bd829f2f78163
Carlos Dario Almonte <c.darioalmonte@gmail.com>
2023-03-13 19:07:16 -0400

    feat(test): add sample snapshot tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,21 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App, {HomeScreen, DetailsScreen} from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('renders App correctly', () => {
   renderer.create(<App />);
+});
+
+it('renders Home correctly', () => {
+  const tree = renderer.create(<HomeScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders Details correctly', () => {
+  const tree = renderer.create(<DetailsScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
