05697d6ca4da7938c2a0460468b37c8f9c508b6a
Carlos Dario Almonte <c.darioalmonte@gmail.com>
2023-03-28 17:27:46 -0400

    feat: de-duplicate common components index files
    Keep contents of javascript index and change file extension to typescript ts
    
    #50

--- src/components/common/index.js
+++
@@ -1,6 +0,0 @@
-import {View, Text, TextInput, Button} from 'react-native';
-
-export {InputWithLabel} from './InputWithLabel';
-export {GoogleSigninButton} from './GoogleSignInBtn';
-export {Touchable} from './Touchable';
-export {View, Text, TextInput, Button};
